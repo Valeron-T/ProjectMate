9090a564422bfdde35a5bd72add465f1dfd73b32
Noel Pinto <noelpinto47@gmail.com>
2023-11-09 02:23:24 +0530

    updateed

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      body: Container(
+        alignment: Alignment.center,
+        child: Row(
+          children: [
+            Container(
+              height: 100.h,
+              width: 20.h,
+              color: Colors.white,
+              alignment: Alignment.topCenter,
+              padding: EdgeInsets.only(top: 2.h),
+              child: Text(
+                "ProjectMate",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 10.sp),
+              ),
+            ),
+            Container(height: 100.h, width: 80.h, color: Colors.lightBlue[50])
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:project_mate/home.dart';
+import 'package:sizer/sizer.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,8 +11,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
+    return Sizer(
+      builder: (context, orientation, deviceType) => MaterialApp(
+        theme: ThemeData.from(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: const Color.fromARGB(255, 173, 229, 255))),
+        debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+      ),
     );
   }
 }
