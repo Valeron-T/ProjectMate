89e591e7c2b9b5d5ddcc8f31563a85e1fe181dd7
Noel Pinto <noelpinto47@gmail.com>
2023-11-21 03:02:12 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_mate/home.dart';
+import 'package:project_mate/views/home.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
---
+++ lib/views/home.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:project_mate/views/Calendar/calendar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  HomePageState createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue[50],
+      body: Container(
+        alignment: Alignment.center,
+        child: const CalendarPage(),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _onTabTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+}
